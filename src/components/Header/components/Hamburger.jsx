ef6bd4f4a39d7f737ab22937f4665affbf6b639e
ashreekar <ashreekar767@gmail.com>
2025-11-16 12:39:39 +0530

    add hamburger menu and logo

--- src/components/Header/components/Hamburger.jsx
+++ src/components/Header/components/Hamburger.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Hamburger() {
   return (
-    <div>Hamburger</div>
+    <div className='p-2 inline-block rounded-full hover:bg-gray-200 cursor-pointer'>
+      <img src="header/burger-menu-svgrepo-com.svg" alt="menu" height="30px" width="30px" />
+    </div>
   )
 }
 
